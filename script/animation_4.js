302f8d3d956a699bf68e37a10dd8396f20a2a6f2
EmilianoTB <emilianotorres28@hotmail.com>
2024-03-26 19:15:35 -0600

    new commit

--- script/animation_4.js
+++ script/animation_4.js
@@ -49,4 +49,19 @@ const new_animation = new IntersectionObserver(entries => {
 
 
 
+  const second_animation = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.search_animation_animation.classList.add('animated'); // Add 'animated' class for CSS
+      } else {
+        // Optionally remove the 'animated' class when out of view (for performance)
+        entry.search_animation_animation.classList.remove('animated');
+      }
+    });
+  });
   
+  // Observe the contact section
+  const new_search_animation = document.querySelector('.text_specialize');
+  hello.observe(new_search_animation);
+
+
